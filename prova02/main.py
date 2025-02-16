7b2f5463c44edb6248fb4b0ef400b86a6d9fe8db
m7zy <144070058+m7zz@users.noreply.github.com>
2025-02-16 11:57:02 -0300

    prova 02 da infinity
    prova 02 da infinity

---
+++ prova02/main.py
@@ -0,0 +1,15 @@
+"""[LPIA-A02] Crie um programa em Python para verificar se um número é positivo, negativo ou zero. O programa deve solicitar ao usuário que insira um número e, em seguida, imprimir uma mensagem indicando a natureza do número.
+
+Se o número for maior que zero, exiba a mensagem "O número é positivo." Se for menor que zero, exiba "O número é negativo." Caso seja zero, a mensagem deve ser "O número é zero."
+    
+Utilize estruturas condicionais e formatação com F-string para criar o programa de forma clara."""
+
+num = float(input("Digite o número escolhido por você: "))
+
+if num > 0:
+    print('Seu número é positivo')
+elif num < 0:
+    print('Seu número é negativo! ')
+else:
+    print('Seu número é zero! ')
+
